bda2625cf94b19f93c8e7225604942fa2e40fe81
Perli Ben David <Perlibiton@gmail.com>
2018-05-25 00:00:41 +0300

    init

---
+++ app/src/main/java/com/example/android/assignment1perli/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.assignment1perli;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_first_project);
+    }
+}
